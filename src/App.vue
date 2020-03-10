2d4ad2e91ca39fb5a2252f07cdda31aae20d14a2
Drew Rawitz <email@drewrawitz.com>
2020-03-10 08:53:39 -0600

    Adding Typekit for access to the Proxima Nova font

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "proxima-nova", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
